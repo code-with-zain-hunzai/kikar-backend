de1b6f9645687d240b1311710522bc4423225398
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-05-29 22:10:51 +0500

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-// import app from "./app";
-// import dotenv from "dotenv";
-// import connectDB from "./config/db";
-// import cors from "cors";
-
-// dotenv.config();
-
-// const PORT = process.env.PORT || 5000;
-
-// connectDB().then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-//   });
-// });
-
 import app from "./app";
+import connectDB from "./config/db";
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 export default app;
+
+// For local development
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
