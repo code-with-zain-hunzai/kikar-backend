318927b34f82da47929c474d6ffbe6dc4415393a
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-05-30 12:11:47 +0500

    fix the server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import serverless from 'serverless-http';
-import app from './app';
+import app from "./app";
+import connectDB from "./config/db";
 
-export const handler = serverless(app);
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+export default app;
+
+if (process.env.NODE_ENV !== "production") {
+  console.log(`Server running on port ${PORT}`);
+}
