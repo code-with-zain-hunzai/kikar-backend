0f339c27f6695367dd4ab635d14a46f60444146a
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-05-30 11:59:16 +0500

    add fix issues

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
+// server.ts
 import app from "./app";
 import connectDB from "./config/db";
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
-
-export default app;
-
-if (process.env.NODE_ENV !== "production") {
+connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-}
+});
