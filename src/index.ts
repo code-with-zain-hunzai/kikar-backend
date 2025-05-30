51b78c04fdea39dac8ce99ecbb10e98e99446d04
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-05-30 11:50:28 +0500

    add and update

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+export default app;
+
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
