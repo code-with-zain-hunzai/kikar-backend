edd871aae38be17ab7ced10af03b18e1139ac0c9
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-05-30 12:02:37 +0500

    chnages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-// server.ts
-import app from "./app";
-import connectDB from "./config/db";
+import serverless from 'serverless-http';
+import app from './app';
 
-const PORT = process.env.PORT || 5000;
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+export const handler = serverless(app);
