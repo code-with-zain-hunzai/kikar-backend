49a1865bfbd0f0c4ce5b5be8f2b97bc4c25456a6
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-05-31 20:39:56 +0500

    fixed deployment issue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import app from "./app";
-import connectDB from "../src/config/db";
+import connectDB from "./config/db";
+
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-export default app;
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
