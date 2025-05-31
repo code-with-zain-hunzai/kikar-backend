0cfd68b6dc7c444fa3c7e5f1e18fdf30b140c273
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-05-31 23:13:37 +0500

    fix some issues

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI_PROD;
+    const uri = process.env.MONGO_URI_LOCAL || process.env.MONGO_URI_PROD;
     const conn = await mongoose.connect(uri as string);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
