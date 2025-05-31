2f69c04d4a73a7f8f059873e78578d95841eaa0f
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-05-31 20:44:58 +0500

    fixed

--- src/api/index.ts
+++
@@ -1,6 +0,0 @@
-import app from "../app";
-import connectDB from "../config/db";
-
-connectDB();
-
-export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import app from "./app";
 import connectDB from "./config/db";
 
-const PORT = process.env.PORT || 5000;
-
 connectDB();
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+export default app;
+
+
+// for local development, we want to run the server
+
+// import app from "./app";
+// import connectDB from "./config/db";
+
+// const PORT = process.env.PORT || 5000;
+
+// connectDB();
+
+// if (process.env.NODE_ENV !== "production") {
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// }
